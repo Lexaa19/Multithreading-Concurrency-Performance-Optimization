e5438846cbb0966e5fb86e1c1014c2cd4a30e837
Caliopi Durdunescu <caliopi.durdunescu@ausy.com>
2022-12-03 15:03:20 +0200

    Add Min Max Metrics solution

---
+++ atomic_operations_volatile_metrics_exercise/Main.java
@@ -0,0 +1,15 @@
+package thread.atomic_operations_volatile_metrics_exercise;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String []args){
+        MinMaxMetrics mmm = new MinMaxMetrics();
+        mmm.addSample(100);
+        //mmm.addSample(200);
+        System.out.println("Minimum "+ mmm.getMin());
+        System.out.println("Maximum " + mmm.getMax());
+        System.out.println("GetClass " + mmm.getClass());;
+    }
+}
---
+++ atomic_operations_volatile_metrics_exercise/MinMaxMetrics.java
@@ -0,0 +1,48 @@
+package thread.atomic_operations_volatile_metrics_exercise;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MinMaxMetrics {
+
+    private volatile long minimum;
+    private volatile long maximum;
+
+    private Object lockObject = new Object();
+
+    /**
+     * Initializes all member variables
+     */
+    public MinMaxMetrics() {
+        minimum = ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE);
+        maximum = ThreadLocalRandom.current().nextLong(Long.MIN_VALUE, 0);
+        System.out.println("Inside constructor minimum: " + minimum);
+        System.out.println("Inside constructor maximum: " + maximum);
+    }
+
+    /**
+     * Adds a new sample to our metrics.
+     */
+    public void addSample(long newSample) {
+        synchronized (lockObject) {
+            this.minimum = Math.min(newSample, this.minimum);
+            this.maximum = Math.max(newSample, this.maximum);
+        }
+        System.out.println("Min is: " + this.minimum);
+        System.out.println("Max is: " + this.maximum);
+    }
+
+    /**
+     * Returns the smallest sample we've seen so far.
+     */
+    public long getMin() {
+        return this.minimum;
+    }
+
+    /**
+     * Returns the biggest sample we've seen so far.
+     */
+    public long getMax() {
+        return this.maximum;
+    }
+}
+
